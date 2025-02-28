4d91dbe8830a3bca393345581111841664ba84e9
zahrafeisal <zahrafeisal3@gmail.com>
2025-03-01 01:02:59 +0300

    crud with sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -12,10 +12,88 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = 'students'
+    __table_args__ = (
+        PrimaryKeyConstraint(
+            'id',
+            name='id_pk'),
+        UniqueConstraint(
+            'email',
+            name='unique_email'),
+        CheckConstraint(
+            'grade BETWEEN 1 AND 12',
+            name='grade_between_1_and_12')
+    )
 
-    id = Column(Integer(), primary_key=True)
+    Index('index_name', 'name')
+
+    id = Column(Integer())
     name = Column(String())
+    email =Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}, " \
+            + f"Grade {self.grade}"
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///:memory:')
     Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    albert_einstein = Student(
+        name = "Albert Einstein",
+        email = "albert.einstein@zurich.edu",
+        grade = 6,
+        birthday = datetime(
+            year = 1879,
+            month = 3,
+            day = 14
+        ),
+    )
+
+    alan_turing = Student(
+        name = "Alan Turing",
+        email = "alan.turing@sherborne.edu",
+        grade = 11,
+        birthday = datetime(
+            year = 1912,
+            month = 6,
+            day = 23
+        ),
+    )
+
+    session.bulk_save_objects([albert_einstein, alan_turing])
+    session.commit()
+
+    # students = session.query(Student)
+    # print([student for student in students])
+    students = session.query(Student).all()
+    print(students)
+
+    names = [name for name in session.query(Student.name)]
+    print(names)
+
+    students_by_grade_desc = [student for student in session.query(
+        Student.name, Student.grade).order_by(desc(Student.grade))]
+    print(students_by_grade_desc)
+
+    oldest_student = [student for student in session.query(
+        Student.name, Student.birthday).order_by(desc(Student.grade)).first()]
+    print(oldest_student)
+
+    query = session.query(Student).filter(Student.name.like('%Alan%'), Student.grade == 11)
+    for record in query:
+        print(record.name)
+
+    session.query(Student).update({
+        Student.grade: Student.grade + 1
+    })
+    print([(
+        student.name,
+        student.grade
+    ) for student in session.query(Student)])
